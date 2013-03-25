9703fc8bda298883b77e4c23638670a3273bcf91
The3gamer <vcbart001@hotmail.com>
2013-03-25 22:06:03 +0100

    Create Server_Core.java
    This Runs the Whole Server and Forge

---
+++ Server_Core.java
@@ -0,0 +1,49 @@
+package net.TechnicLauncher;
+
+import java.io.File;
+import java.util.Logging;
+
+import net.mineecraft.item.ItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.material.Material;
+import net.minecraft.serverProxy;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.TechnicLauncher.Launcher.TLLogger;
+import net.TechincLauncher.Launcher.TLRecord;
+import net.TechnicLauncher.Launcher.TLTechnicSoldier;
+import net.TechincLauncher.Launcher.IMods;
+import net.TechnicLauncher.Launcher.ITechnicSoldier;
+import net.TechnicLauncher.Launcher.IModTL;
+import net.TechnicLauncher.Launcher.IBulid;
+import net.TechnicLauncher.Launcher.IPlugin;
+import net.TechnicLauncher.Launcher.PL.Economy
+import net.TechnicLauncher.Launcher.PL.PL
+import net.TechnicLauncher.Launcher.client.core;
+import net.TechnicLauncher.Launcher.server.core;
+import net.TechnicLauncher.Launcher.server.core.Plugins;
+import net.TechnicLauncher.Launcher.MinecraftForge;
+
+import cpw.mods.common.Mod;
+import cpw.mods.common.Mod.Init;
+import cpw.mods.common.Mod.PostInit;
+import cpw.mods.common.Mod.PreInit;
+import cpw.mods.common.Mod.ServerStarting;
+import cpw.mods.common.SidedProxy;
+import cpw.mods.common.event.FMLInitializationEvent;
+import cpw.mods.common.event.FMLPostInitializationEvent;
+import cpw.mods.common.event.FMLPreInitialzationEvent;
+import cpw.mods.common.event.FMLServerStartingEvent;
+import cpw.mods.common.network.NetworkMod;
+import cpw.mods.common.network.NetworkRegistry;
+import cpw.mods.common.registry.GameRegistry;
+import cpw.mods.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
+
+/**
+*
+* This is not done
+*
+*By The3gamer
+**/
+
